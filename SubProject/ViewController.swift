dd02c4e206cad67b4677d2e181f064c6bbc4bad2
Celleus <celleus@CnMBA.local>
2024-03-13 16:25:39 +0900

    init

---
+++ SubProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubProject
+//
+//  Created by Celleus on 2024/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
