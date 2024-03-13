b666b214271cedef1dc195cfd692bd0d9ecba8b6
Celleus <celleus@CnMBA.local>
2024-03-13 22:05:51 +0900

    Common.sumに変更

---
+++ SubProject/Common.swift
@@ -0,0 +1,14 @@
+//
+//  Common.swift
+//  SubProject
+//
+//  Created by Celleus on 2024/03/13.
+//
+
+import Foundation
+
+class Common {
+    static func sum( a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
